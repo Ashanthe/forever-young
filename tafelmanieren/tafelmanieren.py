82aeacd477c87b120ea8fb31b72a67bf438f1964
Ashanthe <99069097@mydavinci.nl>
2021-09-15 11:01:52 +0200

    forever-young

---
+++ tafelmanieren/tafelmanieren.py
@@ -0,0 +1,15 @@
+a = range(1,11)
+for a in range(1,11):
+    print(a * 8)
+
+
+
+x = range(31,0)
+
+for n in x:
+    print(n)
+
+uur_Am = range(0,13)
+uur_Pm = range(12,24)
+ochtend= "Ochtendtijd is" + uur_Am
+

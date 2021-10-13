57d8c89f26fd7db61a0a359d75a91e24ec4076c9
Ashanthe <99069097@mydavinci.nl>
2021-10-13 11:12:54 +0200

    8 gemaakt

--- robotarm-python-2021-main/example6.py
+++ robotarm-python-2021-main/example6.py
@@ -1,70 +1,24 @@
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 6')
-robotArm.speed = 2
+robotArm.speed = 3
 # Jouw python instructies zet je vanaf hier:
 # Op volgorde
-robotArm.moveRight()
-robotArm.grab()
-for x in range(0,7):
+for i in range(7):
     robotArm.moveRight()
+        
+robotArm.grab()
+robotArm.moveRight()   
 robotArm.drop()
-for x in range(0,8):
+for x in range(8):
     robotArm.moveLeft()
-robotArm.grab()
-robotArm.moveRight()
-robotArm.drop()
-for right in range(0,7):
+    robotArm.grab()
     robotArm.moveRight()
-robotArm.grab()
-for d in range(0,7):
-    robotArm.moveLeft()
-robotArm.drop()
-robotArm.moveRight()
-robotArm.grab()
-robotArm.moveLeft()
-robotArm.drop()
-for k in range(0,2):
-    robotArm.moveRight()
-robotArm.grab()
-for l in range(0,2):
-    robotArm.moveLeft()
-robotArm.drop()
-for c in range(0,3):
-    robotArm.moveRight()
-robotArm.grab()
-for d in range(0,3):    
-    robotArm.moveLeft()
-robotArm.drop()
-for e in range(0,4):
-    robotArm.moveRight()
-robotArm.grab()
-for f in range(0,4):
-    robotArm.moveLeft()
-robotArm.drop()   
-for g in range(0,5):
-    robotArm.moveRight()
-robotArm.grab()
-for h in range(0,5):
-    robotArm.moveLeft()
-robotArm.drop() 
-for i in range(0,4):
-    robotArm.moveRight()
-robotArm.grab()  
-for j in range(0,4):
-    robotArm.moveLeft()
-robotArm.drop()  
-for k in range(0,6):
-    robotArm.moveRight()
-robotArm.grab()
-for l in range(0,6):   
-    robotArm.moveLeft()
-robotArm.grab()
+    robotArm.drop()
+    robotArm.moveLeft()   
 
 
-
-
-
+       
 
 
     
--- robotarm-python-2021-main/example7.py
+++ robotarm-python-2021-main/example7.py
@@ -1,12 +1,17 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 7')
+robotArm.speed = 3
 
 # Jouw python instructies zet je vanaf hier:
-robotArm.moveRight()
-robotArm.grab()
-robotArm.moveLeft()
-robotArm.drop()
+for x in range(5):
+    for move in range (6):
+        robotArm.moveRight()
+        robotArm.grab()
+        robotArm.moveLeft()
+        robotArm.drop()
+    robotArm.moveRight()
+    robotArm.moveRight()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
